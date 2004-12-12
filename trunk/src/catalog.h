b72b0e182677442a4a6d40426b1a3bf28716f12b
stephane <stephane@9af64aeb-9fe1-0310-9449-ccf7ee45ad02>
2004-12-12 19:08:56 +0000

    started python version

--- trunk/src/catalog.h
+++ trunk/src/catalog.h
@@ -8,7 +8,7 @@
  * A search on a catalog.
  *
  * A search starts empty and the query is set
- * piece by piece. It's not possible to reset 
+ * piece by piece. It's not possible to reset
  * the query; create a new search instad.
  *
  * Only one thread should run a search at a
@@ -17,39 +17,39 @@
 struct catalog_search
 {
    /**
-	* Get the query as it is so far.
-	*
-	* @param memory to write the query to. if it's null
-	* nothing will be done
-	* @param len size of dest if it's too small
-	* for the query to fit, it is truncated. The
-	* last bit is always set to 0
-	* @return the real length of the query, always.
-	*/
+    * Get the query as it is so far.
+    *
+    * @param memory to write the query to. if it's null
+    * nothing will be done
+    * @param len size of dest if it's too small
+    * for the query to fit, it is truncated. The
+    * last bit is always set to 0
+    * @return the real length of the query, always.
+    */
    const int (*get_query)(const struct catalog_search *, char *dest, int len);
 
    /**
-	* Append the following string to the query.
-	* @param search 
-	* @param str string to append to the current query
-	* @return true if appending was really done (always true
-	* unless the catalog is frozen, in which case it's always false)
-	*/
+    * Append the following string to the query.
+    * @param search
+    * @param str string to append to the current query
+    * @return true if appending was really done (always true
+    * unless the catalog is frozen, in which case it's always false)
+    */
    bool (*append)(struct catalog_search *search, const char *str);
 
    /**
-	* Freeze the search.
-	*
-	* The most expensive searches won't be executed until
-	* the search is frozen. 
-	*/
+    * Freeze the search.
+    *
+    * The most expensive searches won't be executed until
+    * the search is frozen.
+    */
    void (*freeze)(struct catalog_search *search);
 
    /**
-	* Stop the search and release the resource.
-	* 
-	* This destroys the search object.
-	*/
+    * Stop the search and release the resource.
+    *
+    * This destroys the search object.
+    */
    void (*delete)(struct catalog_search *);
 };
 
@@ -58,13 +58,13 @@ struct catalog_search
  *
  * Search results and sent to this callback whenever
  * they come or whenever the confidence changes.
- * 
+ *
  * Search results can come in any order. The same
  * result may (will) be sent more than once if the
  * confidence increases or decreases. If a catalog
  * notices that a target is totally wrong for the
  * search ats it is now, it should call this function
- * with a confidence of 0. 
+ * with a confidence of 0.
  *
  * A confidence of 1.0 should be reserved for exact
  * matches.
@@ -86,34 +86,42 @@ typedef void (*catalog_search_callback_t)(struct catalog_search *, float confide
  * one thread at a time; there can be several
  * search runnig on paraller threads while
  * an update is running on another. Searches, on
- * the other hand, should only be used by 
+ * the other hand, should only be used by
  * one thread at a time.
  */
-struct catalog 
+struct catalog
 {
 
    /**
-	* Create a new catalog search.
-	* 
-	* This function creates a catalog search, fill in the structure
-	* and returns true. If anything goes wrong, it returns false and
-	* the catalog 
-	*
-	* Catalog search should be released by calling catalog_search->delete(catalog_search)
-	*
-	* @param cb callback to send the results to
-	* @return new catalog search or NULL if something went wrong
-	*/
+    * Create a new catalog search.
+    *
+    * This function creates a catalog search, fill in the structure
+    * and returns true. If anything goes wrong, it returns false and
+    * the catalog
+    *
+    * Catalog search should be released by calling catalog_search->delete(catalog_search)
+    *
+    * @param cb callback to send the results to
+    * @return new catalog search or NULL if something went wrong
+    */
    struct catalog_search (*new_search)(struct catalog *, catalog_search_callback_t cb);
-   
+
    /**
-	* Update the catalog.
-	* 
-	* This operation can take a very long time
-	* and should not be interrupted. Let it run on its own 
-	* low-priority thread. 
-	*/
+    * Update the catalog.
+    *
+    * This operation can take a very long time
+    * and should not be interrupted. Let it run on its own
+    * low-priority thread.
+    */
    void (*catalog_update_t)(struct catalog *);
+
+   /**
+    * Get rid of the catalog.
+    *
+    * The catalog or its structure should not
+    * be used after this call.
+    */
+   void (*delete)(struct catalog *);
 };
 
 
--- trunk/src/catalog/updatedb/updatedb.h
+++ trunk/src/catalog/updatedb/updatedb.h
@@ -1,5 +1,6 @@
 #ifndef CATALOG_UPDATEDB_H
 #define CATALOG_UPDATEDB_H
+#include <stdbool.h>
 #include <catalog.h>
 
 /** \file
@@ -7,13 +8,28 @@
  *
  * This module implements a catalog that does its search using
  * locate. It's a slow method, but one that does not require
- * extra parsing of the catalog. 
+ * extra parsing of the catalog.
  */
 
 /**
- * Create a new catalog structure for 
+ * Create a new catalog structure for
  * searches based on updatedb
  */
 struct catalog *updatedb_new(void);
+
+/**
+ * Check if 'locate' works on this machine.
+ *
+ * This test will fail if locate is not installed
+ * on this machine, if the database does not
+ * exist or if the database is invalid
+ *
+ * The updatedb catalog is useless unless
+ * this test passes.
+ *
+ * @return true if locate works, false
+ * otherwise
+ */
+bool updatedb_is_available(void);
 
 #endif
