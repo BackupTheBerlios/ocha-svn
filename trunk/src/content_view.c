74ebd1f8d1fd85ba1a241984474bf3f10f2930e3
stephane <stephane@9af64aeb-9fe1-0310-9449-ccf7ee45ad02>
2005-03-13 17:49:58 +0000

    FS#10 initial implementation of a content list

---
+++ trunk/src/content_view.c
@@ -0,0 +1,202 @@
+#include "content_view.h"
+
+/** \file Implementation of the API defined in content_view.h
+ *
+ */
+#include "contentlist.h"
+#include <string.h>
+
+/**
+ * The content view structure.
+ */
+struct content_view
+{
+        /** a connection to the catalog */
+        struct catalog *catalog;
+
+        /** This view's main widget */
+        GtkWidget *widget;
+
+        /** Whether this view is attached to anything */
+        gboolean attached;
+
+        /** The source this view is attached to if attached is true */
+        int source_id;
+
+        /** The tree view (may be null, wether or not attached is true) */
+        GtkTreeView *treeview;
+
+        /** The tree model, only if attached (but not always) */
+        ContentList *treemodel;
+};
+
+struct fill_contentlist_userdata
+{
+        ContentList *contentlist;
+        int index;
+        int size;
+};
+/* ------------------------- prototypes: static functions */
+static void init_widget(struct content_view *view);
+static ContentList *run_query(struct content_view *view);
+static gboolean fill_contentlist_cb(struct catalog *catalog,
+                                    float pertinence,
+                                    int entry_id,
+                                    const char *name,
+                                    const char *long_name,
+                                    const char *path,
+                                    int source_id,
+                                    const char *source_type,
+                                    const char *launcher,
+                                    void *userdata);
+
+/* ------------------------- definitions */
+
+/* ------------------------- public functions */
+struct content_view *content_view_new(struct catalog *catalog)
+{
+        struct content_view *retval;
+
+        g_return_val_if_fail(catalog, NULL);
+
+        retval = g_new(struct content_view, 1);
+        memset(retval, sizeof(struct content_view), 0);
+
+        retval->catalog=catalog;
+        init_widget(retval);
+        return retval;
+}
+
+void content_view_destroy(struct content_view *view)
+{
+        g_return_if_fail(view);
+
+        g_free(view);
+}
+
+void content_view_attach(struct content_view *view, int source_id)
+{
+        g_return_if_fail(view);
+        g_return_if_fail(source_id>0);
+
+        if(view->attached && view->source_id==source_id) {
+                return;
+        }
+        if(view->attached) {
+                content_view_detach(view);
+        }
+        g_assert(!view->attached);
+
+        view->source_id=source_id;
+        view->attached=TRUE;
+        view->treemodel=run_query(view);
+        if(view->treemodel) {
+                gtk_tree_view_set_model(view->treeview,
+                                        GTK_TREE_MODEL(view->treemodel));
+        }
+        gtk_widget_show(view->widget);
+}
+
+void content_view_detach(struct content_view *view)
+{
+        g_return_if_fail(view);
+        if(!view->attached) {
+                return;
+        }
+        if(view->treemodel) {
+                gtk_tree_view_set_model(view->treeview, NULL);
+                view->treemodel=NULL;
+        }
+        gtk_widget_hide(view->widget);
+        view->attached=FALSE;
+}
+
+GtkWidget *content_view_get_widget(struct content_view *view)
+{
+        g_return_val_if_fail(view, NULL);
+
+        return view->widget;
+}
+
+/* ------------------------- static functions */
+
+static void init_widget(struct content_view *view)
+{
+        GtkWidget *scroll;
+        GtkWidget *treeview;
+
+        scroll = gtk_scrolled_window_new (NULL, NULL);
+        gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scroll),
+                                        GTK_POLICY_AUTOMATIC,
+                                        GTK_POLICY_AUTOMATIC);
+
+        treeview = gtk_tree_view_new();
+        gtk_widget_show (treeview);
+        gtk_container_add (GTK_CONTAINER (scroll),
+                           treeview);
+        gtk_tree_view_set_headers_visible(GTK_TREE_VIEW(treeview), FALSE);
+        gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(treeview),
+                        0,
+                        "Label",
+                        gtk_cell_renderer_text_new(),
+                        "text", CONTENTLIST_COL_NAME,
+                        NULL);
+        gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(treeview),
+                        1,
+                        "Description",
+                        gtk_cell_renderer_text_new(),
+                        "text", CONTENTLIST_COL_LONG_NAME,
+                        NULL);
+
+        view->treeview=GTK_TREE_VIEW(treeview);
+        view->widget=scroll;
+}
+
+static ContentList *run_query(struct content_view *view)
+{
+        ContentList *retval;
+        struct catalog *catalog = view->catalog;
+        int source_id = view->source_id;
+        guint count = 0;
+        struct fill_contentlist_userdata userdata;
+
+        catalog_get_source_content_count(catalog, source_id, &count);
+        if(count==0) {
+                return NULL;
+        }
+
+        retval = contentlist_new(count);
+        userdata.contentlist = retval;
+        userdata.index=0;
+        userdata.size=count;
+        catalog_get_source_content(catalog,
+                                   source_id,
+                                   fill_contentlist_cb,
+                                   &userdata);
+        return retval;
+}
+
+static gboolean fill_contentlist_cb(struct catalog *catalog,
+                                    float pertinence,
+                                    int entry_id,
+                                    const char *name,
+                                    const char *long_name,
+                                    const char *path,
+                                    int source_id,
+                                    const char *source_type,
+                                    const char *launcher,
+                                    void *_userdata)
+{
+        struct fill_contentlist_userdata *userdata;
+
+        g_return_val_if_fail(_userdata, FALSE);
+
+        userdata = (struct fill_contentlist_userdata *)_userdata;
+        contentlist_set(userdata->contentlist,
+                        userdata->index,
+                        entry_id,
+                        name,
+                        long_name);
+        userdata->index++;
+        return userdata->index<userdata->size;
+}
---
+++ trunk/src/content_view.h
@@ -0,0 +1,55 @@
+#ifndef CONTENT_VIEW_H
+#define CONTENT_VIEW_H
+
+/** \file Manage a view on the content of a source
+ *
+ */
+#include "catalog.h"
+#include <gtk/gtk.h>
+
+/**
+ * Create a new content view and link it with a catalog.
+ *
+ * The view is empty, because it's not linked to any source. Link
+ * it to a source with content_view_attach() and unlink it with
+ * content_view_detach()
+ *
+ * @param catalog a catalog that must stay open for
+ * as long as the view is in use
+ * @return a content view, get the widget using content_view_get_widget()
+ */
+struct content_view *content_view_new(struct catalog *catalog);
+
+/**
+ * Get rid of a view .
+ *
+ * You must call this even though you've destroyed the container
+ * of the widget.
+ *
+ * @param view the view to delete
+ */
+void content_view_destroy(struct content_view *view);
+
+/**
+ * Attach the content view to a source.
+ *
+ * If it was attached to a source, it'll be detached automatically.
+ * @param view
+ * @param source_id source ID
+ */
+void content_view_attach(struct content_view *view, int source_id);
+
+/**
+ * Detach the content view from any source.
+ * @param view
+ */
+void content_view_detach(struct content_view *view);
+
+/**
+ * Get this view's widget
+ * @param view
+ * @return a widget
+ */
+GtkWidget *content_view_get_widget(struct content_view *view);
+
+#endif /* CONTENT_VIEW_H */
